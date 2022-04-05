f349a2b06003460a67eceef2ce3578337362fa0d
Camelron <cam.thebaird@gmail.com>
2022-04-05 00:03:51 +0000

    run.sh updated to run c code in parallel

--- run.sh
+++ run.sh
@@ -1,11 +1,4 @@
 #!/bin/bash 
 sudo tdnf install python3-systemd 
 python3 /home/cameronbaird/journald-stresstest/stresstest.py & 
-
-
-
-
-while true
-do
-	sleep 3
-done
+./home/cameronbaird/journald-stresstest/stresstest_basic
