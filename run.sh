8f9aace1428532d42ed473ce99721a1c9fdce179
Camelron <cam.thebaird@gmail.com>
2022-04-06 17:14:14 +0000

    working stresstest, produces journald crash

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 sudo tdnf install python3-systemd 
 python3 /home/cameronbaird/journald-stresstest/stresstest.py & 
-./home/cameronbaird/journald-stresstest/stresstest_basic
+sudo ./home/cameronbaird/journald-stresstest/stresstest_basic
--- stresstest_basic.c
+++ stresstest_basic.c
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <signal.h>
+#include <pthread.h>
+
+#define MESSAGE_SIZE 75
 
 void entry0() {
     printf("entry0 ran on pid: %d\n", getpid());
@@ -29,17 +32,26 @@ void entry0() {
                         "PAGE_SIZE=%li", sysconf(_SC_PAGESIZE),
                         "N_CPUS=%li", sysconf(_SC_NPROCESSORS_ONLN),
                         NULL);
+	sleep(5);
 }
 
-// should give a value in ms between 0 and 1 seconds 
-int randomvalms() {
+// should give a value in us between 0 and 1 seconds 
+int randomvalus() {
 	float n = (float)rand()/RAND_MAX;
-	n *= 1000;
-	printf("generated: %d\n", (int)n);
+	n *= 1000000;
+	// printf("generated: %d\n", (int)n);
 	return (int)n;
 }
 
+void * killChild(void *args) {
+	usleep(randomvalus());
+	kill(*((pid_t *)args), SIGKILL);
+	return NULL;
+}
+
 int main(int argc, char *argv[]) {
+	uid_t uid = getuid();
+	printf("User id for main is: %d\n", uid);
 	while(true) {
 	pid_t pid = fork();
         if (pid == 0) {
@@ -49,8 +61,31 @@ int main(int argc, char *argv[]) {
         } else {
 	    // main process
 	    // attempt to interrupt the child process to invalidate the pid
-	    usleep(randomvalms());
-	    kill(pid, SIGKILL);
+	    // do this by creating a race condition; 
+	    // pthread will kill the child process with pid: pid after 0-1 second delay
+	    // in the meantime, main thread will try to log about the child thread using OBJECT_PID
+	    // this will, in theory, expose a state where main thread is logging about a process which is killed. 
+	    pthread_t thread_id;
+	    pthread_create(&thread_id, NULL, killChild, (void *)&pid);
+	    char *message_str = (char*)malloc(MESSAGE_SIZE * sizeof(char));
+	    snprintf(message_str, MESSAGE_SIZE, "MESSAGE=loggingmessage about process with pid: %d", pid);
+	    char *objpid_str = (char*)malloc(MESSAGE_SIZE * sizeof(char));
+	    snprintf(objpid_str, MESSAGE_SIZE, "OBJECT_PID=%d", pid);
+	    sd_journal_send(message_str,
+                        	"MESSAGE_ID=52fb62f99e2c49d89cfbf9d6de5e3555",
+                        	"PRIORITY=5",
+				"CODE_FILE=stresstest_basic.c",
+				"CODE_LINE=600",
+				"CODE_FUNC=entry0",
+				"ERRNO=2",
+				"SYSLOG_FACILITY=garbage",
+				"SYSLOG_IDENTIFIER=cloggingcomponent",
+				"SYSLOG_PID=foobar",
+				objpid_str,
+                        	NULL);
+	    free(message_str);
+	    free(objpid_str);
+	    pthread_join(thread_id, NULL);
 	}
         //sleep(5);
     }
